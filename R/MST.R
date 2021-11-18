2e1906ef758ab8fb559b0f1af33835a36d382e82
syntheso <sebsilas@gmail.com>
2021-11-18 12:30:44 +0100

    Remove item_banks directory (this is currently hidden from itembankr due to large size/potentially no longer needed to be used)

--- R/MST.R
+++ R/MST.R
@@ -3,10 +3,10 @@
     prefix = "custom-assets", # custom prefix that will be used to reference your directory
     directoryPath = system.file("www", package = "MST") # path to resource in your package
   )
-  shiny::addResourcePath(
-    prefix = "item_banks", # custom prefix that will be used to reference your directory
-    directoryPath = system.file("item_banks", package = "itembankr") # path to resource in your package
-  )
+  # shiny::addResourcePath(
+  #   prefix = "item_banks", # custom prefix that will be used to reference your directory
+  #   directoryPath = system.file("item_banks", package = "itembankr") # path to resource in your package
+  # )
 }
 
 
