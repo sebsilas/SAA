dcdfc67d3c0c07e06c69ca3f42948949990280fb
syntheso <sebsilas@gmail.com>
2022-05-25 12:25:08 +0200

    Arg updates

--- R/SAA.R
+++ R/SAA.R
@@ -2,41 +2,41 @@
 
 #' Deploy SAA as standalone test
 #'
-#' @param num_items
-#' @param item_bank
-#' @param demographics
-#' @param demo
-#' @param feedback
-#' @param admin_password
-#' @param SNR_test
-#' @param get_range
-#' @param absolute_url
-#' @param examples
-#' @param final_results
-#' @param musicassessr_state
-#' @param store_results_in_db
-#' @param test_username
-#' @param gold_msi
-#' @param with_final_page
-#' @param item_length
-#' @param melody_sound
-#' @param adjust_range
-#' @param test_name
-#' @param show_socials
-#' @param headphones_test
-#' @param get_user_info
-#' @param microphone_test
-#' @param copy_audio_to_location
-#' @param allow_repeat_SNR_tests
-#' @param append_trial_block_before
-#' @param append_trial_block_after
-#' @param stop_recording_after
-#' @param max_goes
-#' @param max_goes_forced
-#' @param long_tone_trials_as_screening
-#' @param long_tone_trials_as_screening_failure_page
-#' @param success_on_completion_page
-#' @param concise_wording
+#' @param num_items The number of items as a list.
+#' @param item_bank The item bank (created with itembankr) to deployed with the test.
+#' @param demographics Deploy demographic form?
+#' @param demo Is demo?
+#' @param feedback Give feedback after trials?
+#' @param admin_password psychTestR admin password.
+#' @param SNR_test Deploy signal-to-noise ratio test?
+#' @param get_range Deploy a test to get the users range at test time and present stimuli accordingly?
+#' @param absolute_url If using online, absolute URL?
+#' @param examples No of examples.
+#' @param final_results Display final results?
+#' @param musicassessr_aws Is this being deployed on AWS via the musicassessr setup?
+#' @param store_results_in_db Store results in a database?
+#' @param test_username Is there a username for the user? This is different from a p_id.
+#' @param gold_msi Deploy Gold-MSI form?
+#' @param with_final_page Should there be a final page? FALSE if there will be more pages in the timeline.
+#' @param item_length What melody lengths should the test be constrained to?
+#' @param melody_sound Sound of melody? e.g, piano.
+#' @param adjust_range Should the range of the user, recorded at test time, be adjusted based on heuristics?
+#' @param test_name Custom name of the test.
+#' @param show_socials Should social media sharing options be shown at the end?
+#' @param headphones_test Should there be a headphone test?
+#' @param get_user_info Grab user info via the browser?
+#' @param microphone_test Deploy a microphone test?
+#' @param copy_audio_to_location Where would you like the audio to be copied to?
+#' @param allow_repeat_SNR_tests Logical. TRUE if participant can fail the SNR test threshold and try again.
+#' @param append_trial_block_before A list of pages to go before the test.
+#' @param append_trial_block_after A list of pages to go after the test.
+#' @param stop_recording_after Stop recording after a certain amount of time.
+#' @param max_goes How many goes can the user have per melody?
+#' @param max_goes_forced Is this forced or optional?
+#' @param long_tone_trials_as_screening Should long tone trials be used as a screening mechanism?
+#' @param long_tone_trials_as_screening_failure_page Where should users be directed to if they fail the long tone screening?
+#' @param success_on_completion_page Where should users be directed to when they complete successfully?
+#' @param concise_wording TRUE for more detailed (but longer) instructions.
 #' @param ...
 #'
 #' @return
@@ -53,10 +53,10 @@ SAA_standalone <- function(num_items = list("long_tones" = 6L,
                            admin_password = "demo",
                            SNR_test = TRUE,
                            get_range = TRUE,
-                           absolute_url = "https://adaptiveeartraining.com",
+                           absolute_url = character(),
                            examples = 2L,
                            final_results = TRUE,
-                           musicassessr_state = "production",
+                           musicassessr_aws = FALSE,
                            store_results_in_db = FALSE,
                            test_username = character(),
                            gold_msi = TRUE,
@@ -69,7 +69,7 @@ SAA_standalone <- function(num_items = list("long_tones" = 6L,
                            headphones_test = TRUE,
                            get_user_info = TRUE,
                            microphone_test = TRUE,
-                           copy_audio_to_location = NULL,
+                           copy_audio_to_location = character(),
                            allow_repeat_SNR_tests = TRUE,
                            append_trial_block_before = psychTestR::module("before"),
                            append_trial_block_after = psychTestR::module("after"),
@@ -92,7 +92,7 @@ SAA_standalone <- function(num_items = list("long_tones" = 6L,
                   absolute_url,
                   examples,
                   final_results,
-                  musicassessr_state,
+                  musicassessr_aws,
                   store_results_in_db,
                   test_username,
                   gold_msi,
@@ -129,7 +129,7 @@ SAA_standalone <- function(num_items = list("long_tones" = 6L,
                                      css = system.file('www/css/musicassessr.css', package = "musicassessr")
                                    ),
                                    languages = c("en"),
-                                   additional_scripts = musicassessr::musicassessr_js(state = musicassessr_state), ...))
+                                   additional_scripts = musicassessr::musicassessr_js(musicassessr_aws = musicassessr_aws, copy_audio_to_location = copy_audio_to_location), ...))
 }
 
 
@@ -151,7 +151,7 @@ SAA_standalone <- function(num_items = list("long_tones" = 6L,
 #' @param absolute_url
 #' @param examples
 #' @param final_results
-#' @param musicassessr_state
+#' @param musicassessr_aws
 #' @param store_results_in_db
 #' @param test_username
 #' @param gold_msi
@@ -190,10 +190,10 @@ SAA <- function(num_items = list("long_tones" = 6L,
                 admin_password = "demo",
                 SNR_test = TRUE,
                 get_range = TRUE,
-                absolute_url,
+                absolute_url = character(),
                 examples = 2,
                 final_results = TRUE,
-                musicassessr_state = "production",
+                musicassessr_aws = FALSE,
                 store_results_in_db = FALSE,
                 test_username = character(),
                 gold_msi = TRUE,
@@ -206,7 +206,7 @@ SAA <- function(num_items = list("long_tones" = 6L,
                 headphones_test = TRUE,
                 get_user_info = TRUE,
                 microphone_test = TRUE,
-                copy_audio_to_location = NULL,
+                copy_audio_to_location = character(),
                 allow_repeat_SNR_tests = TRUE,
                 append_trial_block_before = psychTestR::module("before"),
                 append_trial_block_after = psychTestR::module("after"),
@@ -227,10 +227,10 @@ SAA <- function(num_items = list("long_tones" = 6L,
     is.character(admin_password) & length(admin_password) == 1L,
     is.logical(SNR_test),
     is.logical(get_range) | is.character(get_range) & length(get_range) == 1,
-    is.character(absolute_url) & length(absolute_url) == 1L,
+    is.character(absolute_url),
     is.numeric(examples) & length(examples) == 1L,
     is.logical(final_results),
-    is.character(musicassessr_state) & length(musicassessr_state) == 1L,
+    is.logical(musicassessr_aws),
     is.logical(store_results_in_db),
     is.character(test_username),
     is.logical(gold_msi),
@@ -243,7 +243,8 @@ SAA <- function(num_items = list("long_tones" = 6L,
     is.logical(headphones_test),
     is.logical(get_user_info),
     is.logical(microphone_test),
-    is.null(copy_audio_to_location) | is.character(copy_audio_to_location) & length(copy_audio_to_location) == 1,
+    is.character(copy_audio_to_location),
+    length(copy_audio_to_location) == 1 & musicassessr_aws == FALSE,
     is.logical(allow_repeat_SNR_tests),
     is.list(append_trial_block_before) | psychTestR::is.timeline(append_trial_block_before),
     is.list(append_trial_block_after) | psychTestR::is.timeline(append_trial_block_after),
@@ -271,7 +272,6 @@ SAA <- function(num_items = list("long_tones" = 6L,
                                      absolute_url,
                                      test_username,
                                      store_results_in_db,
-                                     musicassessr_state,
                                      adjust_range,
                                      headphones_test,
                                      get_user_info,
@@ -343,21 +343,12 @@ SAA <- function(num_items = list("long_tones" = 6L,
 
 
 
-
-
-
-
-
-
-
-
 SAA_intro <- function(demo = FALSE,
                       SNR_test = TRUE,
                       get_range = TRUE,
-                      absolute_url,
+                      absolute_url = character(),
                       test_username = NULL,
                       store_results_in_db = FALSE,
-                      musicassessr_state = "production",
                       adjust_range = TRUE,
                       headphones_test,
                       get_user_info,
@@ -583,6 +574,9 @@ final_results_saa <- function(test_name,
 
 
 # SAA_standalone(num_items = list(long_tones = 1L, arrhythmic = 2L, rhythmic = 2L),
-#                SNR_test = F, get_range = F,  musicassessr_state = "test", examples = 0,
+#                SNR_test = F, get_range = F,  musicassessr_aws = FALSE, examples = 0,
 #                copy_audio_to_location = "/Users/sebsilas/Desktop/audio_test")
+
+
+# musicassessr::musicassessr_js(musicassessr_aws = FALSE, copy_audio_to_location = "/Users/sebsilas/Desktop/audio_test")
 
