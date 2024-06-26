999dbceefd845cf5ea7fce0326bb9a16da1a1ea3
sebsilas <sebsilas@gmail.com>
2024-06-26 11:35:39 +0000

    Image

--- R/SAA.R
+++ R/SAA.R
@@ -537,7 +537,8 @@ SAA <- function(app_name,
                                                                   feedback = feedback,
                                                                   sound = melody_sound,
                                                                   page_text = shiny::tags$div(
-                                                                    shiny::tags$img(src = "https://musicassessr.com/assets/img/singing.png", height = 100, width = 100),
+                                                                    shiny::tags$img(id = "singImage",
+                                                                                    src = "https://musicassessr.com/assets/img/singing.png", height = 100, width = 100),
                                                                     shiny::tags$br(),
                                                                     shiny::tags$p(psychTestR::i18n("sing_melody_page_text"))
                                                                   ),
@@ -560,7 +561,7 @@ SAA <- function(app_name,
                                                                 feedback = feedback,
                                                                 sound = melody_sound,
                                                                 page_text = shiny::tags$div(
-                                                                  shiny::tags$img(src = "https://musicassessr.com/assets/img/singing.png", height = 100, width = 100),
+                                                                  shiny::tags$img(id = "singImage", src = "https://musicassessr.com/assets/img/singing.png", height = 100, width = 100),
                                                                   shiny::tags$br(),
                                                                   shiny::tags$p(psychTestR::i18n("sing_rhythmic_melodies_page_text"))
                                                                 ),
